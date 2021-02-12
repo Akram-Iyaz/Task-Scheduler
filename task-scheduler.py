a1bb360e76c723cdc8d1b3ff05f8a8e0881f9ec1
Akram Iyaz <63152553+Akram-Iyaz@users.noreply.github.com>
2021-02-12 23:18:31 +0530

    Update task-scheduler.py

--- task-scheduler.py
+++ task-scheduler.py
@@ -1,12 +1,15 @@
 import os,time
 from time import strftime
-os.system('#!/usr/bin/zsh')
+os.system('#!/usr/bin/zsh;zsh')
+os.system('#!~/.zshrc')
+os.system('source /usr/SHELL/.appeal.zsh')
 job=input("Enter the Command you want to execute: ")
-print("On what time you want to execute the task!, time format=HH:MM:SS")
+print("On what time you want to execute the task!\nTime format=HH:MM:SS\nNOTE ':' will be added automatically\n")
 task_time=input("24 hour time:  ")
-while strftime("%H:%M:%S") != task_time:
+task_time=task_time[0:2]+":"+task_time[2:4]+":"+task_time[4:]
+while True:
 	if strftime("%H:%M:%S") == task_time:
 		os.system(job)
-		time.sleep(5)
+		time.sleep(2)
 		break
 		
